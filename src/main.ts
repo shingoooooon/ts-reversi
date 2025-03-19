8b956b2a1cd001218b538e5e6c640bdbe3a84e5d
shingoooooon <skksb75@gmail.com>
2025-03-19 12:10:25 +0900

    setup express

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import morgan from "morgan";
+import "express-async-errors";
+
+const PORT = 3000;
+const app = express();
+
+app.use(morgan("dev"));
+
+app.get("/api/hello", async (req, res) => {
+    res.json({ message: "Hello Express" });
+});
+
+app.get("/api/error", async (req, res) => {
+    throw new Error("test error");
+});
+
+app.use(ErrorHandler);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}: http://localhost:${PORT}`);
+});
+
+function ErrorHandler(
+    err: Error,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+) {
+    console.error("Unexpected error occurred", err);
+    res.status(500).json({ message: "Internal Server Error" });
+}

083cdbd366ed3f07b4abbdd16aa87a19cf103555
shingoooooon <skksb75@gmail.com>
2025-03-23 18:00:48 +0900

    inplement basic reversi function

--- static/main.js
+++ static/main.js
@@ -1,53 +1,73 @@
-console.log("Hello, JS!");
+const EMPTY = 0
+const DARK = 1
+const LIGHT = 2
 
-const EMPTY = 0;
-const DARK = 1;
-const LIGHT = 2;
+const boardElement = document.getElementById('board')
 
-const INITIAL_BOARD = [
-    [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-    [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-    [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-    [EMPTY, EMPTY, EMPTY, DARK, LIGHT, EMPTY, EMPTY, EMPTY],
-    [EMPTY, EMPTY, EMPTY, LIGHT, DARK, EMPTY, EMPTY, EMPTY],
-    [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-    [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-    [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-]
+async function showBoard(turnCount) {
+  const response = await fetch(`/api/games/latest/turns/${turnCount}`)
+  const responseBody = await response.json()
+  const board = responseBody.board
+  const nextDisc = responseBody.nextDisc
 
-const boardElement = document.querySelector(".board");
+  while (boardElement.firstChild) {
+    boardElement.removeChild(boardElement.firstChild)
+  }
 
-async function showBoard() {
-    boardElement.innerHTML = "";
-    
-    INITIAL_BOARD.forEach((row, rowIndex) => {
-        row.forEach((cell, colIndex) => {
-            const cellElement = document.createElement("div");
-            cellElement.classList.add("square");
-            cellElement.dataset.row = rowIndex;
-            cellElement.dataset.col = colIndex;
-            boardElement.appendChild(cellElement);
+  board.forEach((line, y) => {
+    line.forEach((square, x) => {
+      // <div class="square">
+      const squareElement = document.createElement('div')
+      squareElement.className = 'square'
 
-            if (cell !== EMPTY) {
-                const stoneElement = document.createElement("div");
-                const color = cell === DARK ? "dark" : "light";
-                stoneElement.classList.add("stone", color);
-                cellElement.appendChild(stoneElement);
-            }
-        });
-    });
+      if (square !== EMPTY) {
+        // <div class="stone dark">
+        const stoneElement = document.createElement('div')
+        const color = square === DARK ? 'dark' : 'light'
+        stoneElement.className = `stone ${color}`
 
+        squareElement.appendChild(stoneElement)
+      } else {
+        squareElement.addEventListener('click', async () => {
+          const nextTurnCount = turnCount + 1
+          await registerTurn(nextTurnCount, nextDisc, x, y)
+          await showBoard(nextTurnCount)
+        })
+      }
+
+      boardElement.appendChild(squareElement)
+    })
+  })
 }
 
 async function registerGame() {
-    await fetch("/api/games", {
-        method: "POST",
-    });
+  await fetch('/api/games', {
+    method: 'POST'
+  })
 }
 
-function main() {
-    registerGame();
-    showBoard();
+async function registerTurn(turnCount, disc, x, y) {
+  const requestBody = {
+    turnCount,
+    move: {
+      disc,
+      x,
+      y
+    }
+  }
+
+  await fetch('/api/games/latest/turns', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(requestBody)
+  })
 }
 
-main();
+async function main() {
+  await registerGame()
+  await showBoard(0)
+}
+
+main()

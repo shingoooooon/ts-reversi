41f556a7684bf51678ff0ae2889396b0336192bb
shingoooooon <skksb75@gmail.com>
2025-04-04 07:32:57 +0900

    add domain model method

--- src/domain/move.ts
+++ src/domain/move.ts
@@ -3,4 +3,12 @@ import { Point } from "./point";
 
 export class Move {
     constructor(private _disc: Disc, private _point: Point) {}
-}
+
+    get disc() {
+        return this._disc
+    }
+
+    get point() {
+        return this._point
+    }
+ }
